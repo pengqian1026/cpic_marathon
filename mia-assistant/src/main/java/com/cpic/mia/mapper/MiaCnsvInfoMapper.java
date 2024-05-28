ac49502b1f097d0aa959fcc86c490b4ec6c08020
xiaoyan <936556939@qq.com>
2024-05-28 21:40:07 +0800

    修改注释

--- mia-assistant/src/main/java/com/cpic/mia/mapper/MiaCnsvInfoMapper.java
+++ mia-assistant/src/main/java/com/cpic/mia/mapper/MiaCnsvInfoMapper.java
@@ -6,11 +6,11 @@ import com.cpic.mia.domain.MiaCnsvInfoPO;
 import java.util.List;
 
 /**
-* @author pengqian-012
-* @description 针对表【mia_cnsv_info(稽核对话信息记录表)】的数据库操作Mapper
-* @createDate 2024-05-26 16:59:26
-* @Entity com.cpic.mia.domain.MiaCnsvInfoPO
-*/
+ * @author pengqian-012
+ * @description 针对表【mia_cnsv_info(稽核对话信息记录表)】的数据库操作Mapper
+ * @createDate 2024-05-26 16:59:26
+ * @Entity com.cpic.mia.domain.MiaCnsvInfoPO
+ */
 public interface MiaCnsvInfoMapper extends BaseMapper<MiaCnsvInfoPO> {
 
     List<MiaCnsvInfoPO> getHistoryCnsv(String cnsvId);
